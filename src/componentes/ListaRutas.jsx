81faa920730df90995f1e25e38fe1e817bd7710f
juancasanchez6 <jcsanchezr04@educarex.es>
2024-03-06 13:12:49 +0100

    Actualizacion de tfg

---
+++ src/componentes/ListaRutas.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ListaRutas = () => {
+  return (
+    <div>
+      <h2>Rutas Disponibles</h2>
+      {/* Lista de rutas */}
+    </div>
+  );
+};
+
+export default ListaRutas;
